5c52c73725ce3208c19bd1dbf20102a07e9258c0
LucaCattaneoo <127226638+LucaCattaneoo@users.noreply.github.com>
2023-04-20 15:21:12 +0200

    Update Main.java

--- Gruppo_4/src/Gruppo_4/Main.java
+++ Gruppo_4/src/Gruppo_4/Main.java
@@ -3,40 +3,29 @@ package Gruppo_4;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner;
-
-		scanner = new Scanner(System.in);
+		int numeroGiocatori;
+		String nickname;
+		int numeroDelGiocatore=1;
+		Scanner sc = new Scanner(System.in);
 		
-		int numGiocatori;
+		do {
+			System.out.println("Quante persone giocano? [2,3 o 4]");
+			numeroGiocatori = sc.nextInt();
+		}while(numeroGiocatori<2||numeroGiocatori>4);
 		
-		do{
-			System.out.println("inserisci numero di giocatori (massimo 4): ");
-			numGiocatori = scanner.nextInt();
+		Giocatore[] g = new Giocatore[numeroGiocatori];
+		
+		for(int i=0;i<numeroGiocatori;i++) {
+			System.out.println("Inserisci il nickname del giocatore " + numeroDelGiocatore);
+			numeroDelGiocatore++;
+			nickname = sc.next();
+			g[i] = new Giocatore(nickname, numeroGiocatori);
 		}
-		while(numGiocatori>=5);
 		
-		
-		//String giocatore1 = scanner.nextLine();
-		
-		System.out.println("inserisci nome giocatore 1 : " );
-		Giocatore giocatore1 = new Giocatore ("", 1);
-		System.out.println(giocatore1);
-		
-		System.out.println("inserisci nome giocatore 2 : " );
-		Giocatore giocatore2 = new Giocatore ("", 2);
-		System.out.println(giocatore2);	
-
-		System.out.println("inserisci nome giocatore 3 : " );
-		Giocatore giocatore3 = new Giocatore ("", 3);
-		System.out.println(giocatore3);	
-
-		
-		System.out.println("inserisci nome giocatore 4 : " );
-		Giocatore giocatore4 = new Giocatore ("", 4);
-		System.out.println(giocatore4);	
-		
-		
-		scanner.close();
+		System.out.println("PARTECIPANTI");
+		for(int q=0;q<numeroGiocatori;q++) {
+			System.out.println(g[q].toString());
+		}
+		sc.close();
 	}
-
 }
