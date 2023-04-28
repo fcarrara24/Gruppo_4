aa250dc1288f0218bdfd33cb2546c2dfd0158e92
fcarrara24 <114650486+fcarrara24@users.noreply.github.com>
2023-04-28 22:52:49 +0200

    completamento Grid
    ho praticamente* terminato e testato la classe grid:
    
    -void caselleSeparate()->
    	da attivare ogni volta che si prende qualcosa dalla Grid, 
    	fa automaticamente il refresh della grid se risulta
    	che tutti i tasselli sono separate
    
    
    -int[] prendiColori:
    	crea una"mano" di tasselle da inserire nella propria libreria
    		(si può accedere a mano anche tramite l'attributo hand[], 
    		che riprende gli ultimi dati salvati con la funzione prendiColori
    
    *bisogna aggiungere l'algoritmo di pesca senza ripetizione di ciascun tassello
    	------------
    rispetto all'UML di partenza mi sono permesso le seguenti modifiche:
    
    -Grid(numeroGiocatori)	
    	la creazione di una nuova Grid 
    	viene gestita da un apposito costruttore 
    
    -void ripristinaTessereTolte(int tessereRifiutate)
    	nel caso in cui la libreria personale sia piena,
    	e quindi si siano riuscte ad inserire solo 1 o solo 2 
    	tasselli, con questa funzione ripristino i tasselli tolti extra
    
    	------------
    rimane invariata la classe pritGrid(), mentre la getGeneralGrid è 
    stata testata per funzionare sia quando si crea una nuova grid (funzionalità precedente), 
    sia per aggiornare qualla vecchia
    																																																													
    
    
    infine, in fondo trovate il "finto main" (da rimuovere alla pubblicazione) sul quale ho testato le nuove funzionalità

--- Gruppo_4/src/Gruppo_4/Grid.java
+++ Gruppo_4/src/Gruppo_4/Grid.java
@@ -1,161 +1,409 @@
 package Gruppo_4;
+import java.util.Scanner;
+
+
 
 public class Grid {
+			
+			private static int row = 10;
+			private static int column= 10;
+			
+			int [][] grid=new int [row][column];
+			
+			int [] hand=new int [2];
+			private int player;
+			private int temporaryValue2=0;   //salvo le carte in caso di libreria piena
+			private int temporaryValue3=0;
+			private int[] temporaryX=new int[2];
+			private int[] temporaryY=new int[2];
 	
-	//per implementarla all'interno del main o in un'altra funzione:
-	
-	//Grid grid= new Grid();
-	//int player = 2; // creare sistema per rilevare giocatori
-	//grid.printGrid(grid.getGeneralGrid(player));
-	
-	private static int row = 10;
-	private static int column= 10;
-	
-	int [][] grid=new int [row][column];
 	
 	
-	int[][] getGeneralGrid(int player){
-		for (int i=1; i<=9; i++) {
-			for (int j=1; j<=9; j++) {
-				
-				
-				if (isPlaceRight (player,  i,  j)) {
-					this.grid [i][j] = (int) (Math.random() * 6) +1;
-					
-				} else {
-					this.grid [i][j] = 0;
-				}
+	
+	public Grid(int Nplayer)
+	{
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				this.grid[i][j] = 0; 
 			}
 		}
-		
-		return grid;
+		this.player=Nplayer;
+		getGeneralGrid();
 	}
-	
-	
-	private boolean isPlaceRight (int player, int i, int j) {
-		switch(player) {
-			case 4:
-				if (!adapt4Players (i, j))
+		
+		
+		
+		
+		//grigliaGenerale
+		
+	public int[][] getGeneralGrid(){
+			for (int i=1; i<=9; i++) {
+				for (int j=1; j<=9; j++) {
+					
+					
+					if (isPlaceRight (i,  j)&&(this.grid[i][j]==0)) {
+						this.grid [i][j] = (int) (Math.random() * 6) +1;
+						
+					}
+				}
+			}
+			
+			return grid;
+		}
+		
+		//seleziona il numero di giocaori per gestire la griglia
+		private boolean isPlaceRight (int i, int j) {
+			switch(this.player) {
+				case 4:
+					if (!adapt4Players (i, j))
+						return false;
+					break;
+				case 3:
+					if (!adapt3Players (i, j))
+						return false;
+					break;
+				case 2:
+					if (!adapt2Players (i, j))
+						return false;
+					break;
+				default:
+					System.out.println("errore: hai selezionato un numero di giocatori non compreso tra 2 e 4");
 					return false;
-				break;
-			case 3:
-				if (!adapt3Players (i, j))
-					return false;
-				break;
-			case 2:
-				if (!adapt2Players (i, j))
-					return false;
-				break;
-			default:
-				return false;
+			}
+			
+			
+			return true;
 		}
 		
 		
-		return true;
-	}
-	
-	private boolean adapt4Players (int i, int j){
 		
-				
-				//inizio controlli e nullificazione cifre
+		
+		private boolean adapt4Players (int i, int j){
 			
-				if (i==1 && (j!=4 && j!=5)) {
+					
+					//inizio controlli e nullificazione cifre
+				
+					if (i==1 && (j!=4 && j!=5)) {
+							return false;
+						}
+					
+					
+					if (i==2 && (j==1||j==2||j==3||j==7||j==8||j==9)) {
+						return false; 
+					}
+					
+					if ((i==3||i ==7) && (j==1||j==2||j==8||j==9))
+						return false;
+					
+					if (i==4 && j==1) {
 						return false;
 					}
+					
+					if (i==6 && j==9) {
+						return false;
+					}
+					
+					if (i==8 && (j==1||j==2||j==3||j==7||j==8||j==9)) {
+						return false; 
+					}
+					
+					if (i==9 && (j==1||j==2||j==3||j==4||j==7||j==8||j==9)) {
+						return false;
+					}
+					
+					
 				
-				
-				if (i==2 && (j==1||j==2||j==3||j==7||j==8||j==9)) {
-					return false; 
-				}
-				
-				if ((i==3||i ==7) && (j==1||j==2||j==8||j==9))
+			return true;
+	}
+		
+		
+		
+	private boolean adapt3Players(int i, int j) {
+		if (adapt4Players(i, j)) {
+
+			if (i == 5 && (j == 1 || j == 9)) {
 				return false;
-				
-				if (i==4 && j==1) {
-					return false;
-				}
-				
-				if (i==6 && j==9) {
-					return false;
-				}
-				
-				if (i==8 && (j==1||j==2||j==3||j==7||j==8||j==9)) {
-					return false; 
-				}
-				
-				if (i==9 && (j==1||j==2||j==3||j==4||j==7||j==8||j==9)) {
-					return false;
-				}
-				
-				
-			
-		return true;
-}
-	
-	private boolean adapt3Players (int i, int j) {
-		if (adapt4Players (i, j)) {
-			
-			if (i==5 && (j==1||j==9)) {
+			}
+
+			if (j == 5 && (i == 1 || i == 9)) {
 				return false;
+			}
+			if (i == 2 && j == 6) {
+				return false;
+			}
+			if (i == 8 && j == 4) {
+				return false;
+			}
+			if (i == 4 && j == 1) {
+				return false;
+			}
+			if (i == 6 && j == 9) {
+				return false;
+			}
+
+			return true;
+
+		}
+		return false;
+	}
+
+		private boolean adapt2Players (int i, int j) {
+			if (adapt3Players (i, j)) {
+				if ((i==3 || i ==7) && (j==3|| j==7)) {
+					return false;
+				}
+				if (i==1 && j==4) {
+					return false;
+				}
+				if (i==9 && j==6) {
+					return false;
+				}
+				if (i==4 && j==9) {
+					return false;
+				}
+				if (i==6 && j ==1) {
+					return false;
+				}	
+				
+				return true;
 			}
 		
-			if (j==5 && (i==1||i==9)) {
-				return false;
+			return false;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		private boolean isSideFree(int x, int y) {
+			if (this.grid[x+1][y]==0) {
+				return true;
 			}
-			if (i==2 && j==6) {
-				return false;
+			if (this.grid[x-1][y]==0) {
+				return true;
 			}
-			if (i==8 && j==4) {
-				return false;
+			if (this.grid[x][y+1]==0) {
+				return true;
 			}
-			if (i==4 && j==1) {
-				return false;
+			if (this.grid[x][y-1]==0) {
+				return true;
 			}
-			if (i==6 && j==9) {
-				return false;
+			return false;
+		}
+		
+		private boolean allSideFree(int x, int y) {
+			if (this.grid[x+1][y]==0 && this.grid[x-1][y]==0 && this.grid[x][y+1]==0 && this.grid[x][y-1]==0) {
+				return true;
 			}
-			
-			return true;
+			return false;
+		}
+		
+
+		 
+		public void ripristinaTessereTolte(int tessereRifiutate) {
+			if(tessereRifiutate==2) {
+				this.grid[this.temporaryX[1]][this.temporaryY[1]]=this.temporaryValue3;
+			}
+				
+			if(tessereRifiutate==2||tessereRifiutate==1) {
+				this.grid[this.temporaryX[0]][this.temporaryY[0]]=this.temporaryValue2;
+			}
+			else {
+				System.out.println("attenzione: non puoi rifiutare un numero di carte diverse da 1 o 2 ");
+			}
+				
 			
 		}
-		return false;
-	}
-
-	private boolean adapt2Players (int i, int j) {
-		if (adapt3Players (i, j)) {
-			if ((i==3 || i ==7) && (j==3|| j==7)) {
-				return false;
+		
+public int[] prendiColori() {
+			
+			int[] hand= new int[3];
+			int x;
+			int y;
+			int scelta;
+			Scanner sc1=new Scanner(System.in);
+			Scanner sc= new Scanner(System.in);
+			
+			
+			
+			//primo tassello
+			
+			do {
+			
+			System.out.println("\ninserire la coordinata verticale della casella da prendere ");
+			x=sc1.nextInt();
+			System.out.println("inserire la coordinata orizzontale della casella da prendere ");
+			y=sc1.nextInt();
+			
+			if(this.grid[x][y]==0)
+				System.out.println("Hai preso una casella vuota.\n Riscrivi le coordinate corrette ");
+			if(!isSideFree(x,y))
+				System.out.println("Questa casella non ha lato libero.\n Riscrivi le coordinate corrette");
+			} while (this.grid[x][y]==0 || !(isSideFree(x,y)));
+										//per essere presa deve esistere e avere almeno 1 lato libero
+			
+			hand[0]=this.grid[x][y];
+			this.hand=hand;
+			this.grid[x][y]=0;			
+			
+			//continuare o terminare
+			System.out.print("prendere il secondo elemento? (<1> per continuare)");
+			scelta=sc.nextInt();
+			
+			
+			if(scelta!=1) { 
+				sc.close();
+				System.out.println("hai preso 1 elemento: "+hand[0]);
+				return hand;
 			}
-			if (i==1 && j==4) {
-				return false;
+				do {
+				System.out.println("scegliere se prendere l'elemento sopra(2), sotto(3), a destra(4) o a sinistra(5)");
+					scelta=sc.nextInt();
+				if(scelta==2&&this.grid[x-1][y]!=0) {
+					x=x-1;
+				}
+				if(scelta==3&&this.grid[x+1][y]!=0) {
+					x=x+1;
+				}
+				if(scelta==4&&this.grid[x][y+1]!=0) {
+					y=y+1;
+				}
+				if(scelta==5&&this.grid[x][y-1]!=0) {
+					y=y-1;
+				}
+				if(this.grid[x][y]==0) {
+					System.out.println("non hai scritto correttamente, \n oppure hai preso un elemento vuoto");
+				}
+			} while (this.grid[x][y]==0);
+			
+			
+			hand[1]=this.grid[x][y];
+			this.hand=hand;
+			this.temporaryValue2=hand[1];
+			this.temporaryX[0]=x;
+			this.temporaryY[0]=y;			//salvo il codice in caso di libreria piena
+			this.grid[x][y]=0;				//cancello
+			
+			//continuare
+			System.out.println("prendere il terzo elemento? (<1> per continuare)\n");
+			int continua=sc.nextInt();
+			
+			
+			if(continua!=1) {
+				sc.close();
+				System.out.println("\nhai preso 2 elementi: "+hand[1]+" e "+hand[2]);
+				return hand;
 			}
-			if (i==9 && j==6) {
-				return false;
+			
+			
+			//-------------pesca 3 elementi
+			
+			if(scelta==3&&this.grid[x+1][y]!=0) {
+					x=x+1;
+					hand[2]=this.grid[x][y];
+					this.hand=hand;
+					this.temporaryValue3=hand[2];
+					this.temporaryX[1]=x;
+					this.temporaryY[1]=y;			//salvo il codice in caso di libreria piena
+					this.grid[x][y]=0;				//cancello l'elemento
 			}
-			if (i==4 && j==9) {
-				return false;
+			
+			else if(scelta==2&&this.grid[x-1][y]!=0) {
+					x=x-1;
+					hand[2]=this.grid[x][y];
+					this.hand=hand;
+					this.temporaryValue3=hand[2];
+					this.temporaryX[1]=x;
+					this.temporaryY[1]=y;			//salvo il codice in caso di libreria piena
+					this.grid[x][y]=0;				//cancello l'elemento
 			}
-			if (i==6 && j ==1) {
-				return false;
+			
+			else if(scelta==4&&this.grid[x][y+1]!=0) {
+					y=y+1;
+					hand[2]=this.grid[x][y];
+					this.hand=hand;
+					this.temporaryValue3=hand[2];
+					this.temporaryX[1]=x;
+					this.temporaryY[1]=y;			//salvo il codice in caso di libreria piena
+					this.grid[x][y]=0;				//cancello l'elemento
 			}	
 			
-			return true;
+			else if(scelta==5&&this.grid[x][y-1]!=0) {
+					y=y-1;
+					hand[2]=this.grid[x][y];
+					this.hand=hand;
+					this.temporaryValue3=hand[2];
+					this.temporaryX[1]=x;
+					this.temporaryY[1]=y;			//salvo il codice in caso di libreria piena
+					this.grid[x][y]=0;				//cancello l'elemento
+			}
+			
+			
+			
+			sc.close();
+			if(hand.length==2)
+				System.out.println("\nhai preso 2 elementi: "+hand[0]+" e "+hand[1]);
+			else
+				System.out.println("\nhai preso 3 elementi: "+hand[0]+", "+hand[1]+" e "+hand[2]);
+			return hand;
+			
 		}
-	
-		return false;
-	}
-	
-	
-	
-	
-	
-	public void printGrid(int grid[][]) {
-		for (int i=1; i<=9; i++) {
-			System.out.print("\n"+(i)+": ");
-			for (int j=1; j<=9; j++) {
-					System.out.print(grid[i][j]+" ");
+			
+		
+		
+		
+		
+		
+		
+		public void caselleSeparate() { //contiene anche il refresh della griglia
+			for (int i=1; i<=9; i++) {
+				for (int j=1; j<=9; j++) {
+					if (allSideFree(i, j)) {
+						return;
+					}
+				}
+			}
+			getGeneralGrid();
+		}
+
+		public void printGrid() {
+			System.out.println();
+			for (int i = 1; i <= 9; i++) {
+				System.out.print("\n" + (i) + ": ");
+				for (int j = 1; j <= 9; j++) {
+					System.out.print(this.grid[i][j] + " ");
 				}
 			}
 		}
-}
+		
+		
+	}
 
+
+
+
+
+/* package Gruppo_4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int player=4;
+		Grid grid=new Grid(player);
+		grid.printGrid();
+		grid.prendiColori();
+		int[] vettore=grid.hand;
+		for(int i=0; i<grid.hand.length; i++) {
+			System.out.println(" -> vettore "+i+" "+vettore[i]);
+		}
+		grid.printGrid();
+		grid.ripristinaTessereTolte(2);
+		grid.printGrid();
+
+	}
+
+}*/
