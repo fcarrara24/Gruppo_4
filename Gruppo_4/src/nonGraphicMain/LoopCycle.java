e7ca8b66c1b6876b654e5fba4373d8bf4921dc47
francesco carrara <f.carrara24@studenti.unibg.it>
2023-05-29 11:46:15 +0200

    aggiunta sezione relativa al regolamento del gioco, da competare inserendo la lista degli obiettivi comuni (contenente le regole in formato stringa) e la lista degli obiettivi personali (contenente il pattern nella griglia da rispettare e il sistema di punteggio)
    a discrezione dell'autore stampare la griglia generale o specializzata
    in base all'obiettivo comune e personale attuale

--- Gruppo_4/src/nonGraphicMain/LoopCycle.java
+++ Gruppo_4/src/nonGraphicMain/LoopCycle.java
@@ -1,6 +1,8 @@
 package nonGraphicMain;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import classes.*;
 
 public class LoopCycle {
@@ -10,7 +12,7 @@ public class LoopCycle {
 	public int numObComune;
 	public boolean flagObComuniAttivi;
 	public OrdineObComune ordineObComune;
-	
+	public Regolamento regolamento= new Regolamento();
 	
 	private Giocatore giocatoreAttuale;
 	
@@ -29,6 +31,9 @@ public class LoopCycle {
 	}
 	
 	boolean Funzione() {
+		Scanner sc=new Scanner(System.in);
+		sc.reset();
+		System.out.println("obiettivo comune numero"+numObComune);
 		
 		System.out.println("tavolo: \n");
 		tavolo.printGrid();
@@ -38,8 +43,14 @@ public class LoopCycle {
 		} else {
 			giocatoreAttuale=giocatori.get(((turno+1)%giocatori.size())-1);
 		}
-		System.out.println("\n"+giocatoreAttuale.nome+", seleziona la colonna della tua libreria dove mettere gli elementi tra le seguenti ");
 		
+		System.out.println(giocatoreAttuale.nome+", il tuo obiettivo personale è il numero "+giocatoreAttuale.obiettivoPersonale.numero
+						   +" scrivere -true- per vedere il regolamento, -false- per saltarlo");
+		boolean scelta=sc.nextBoolean();
+		if(scelta) {
+			regolamento.listaObPersonali();
+		}
+		System.out.println("\n"+giocatoreAttuale.nome+", seleziona la colonna della tua libreria dove mettere gli elementi tra le seguenti ");
 		
 		int colonnaLibreria=this.giocatoreAttuale.shelf.selezioneColonna();
 		int maxColonne=this.giocatoreAttuale.shelf.getMax(colonnaLibreria);
